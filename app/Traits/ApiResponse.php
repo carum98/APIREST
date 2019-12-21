1267ea44825cff4a58323c66e71ebe079856c017
Carlos Umaña Acevedo <carumtito@gmail.com>
2019-12-21 15:30:11 -0600

    Operaciones complejas Buyer

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -3,7 +3,8 @@
 namespace App\Traits;
 
 
-use Illuminate\Database\Eloquent\Collection;
+//use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 trait ApiResponse
