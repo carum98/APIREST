88e1ff51bbed8066db014ed6cedffca73da118f1
Carlos Umaña Acevedo <carumtito@gmail.com>
2019-12-24 11:38:45 -0600

    Pagination

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -4,8 +4,10 @@ namespace App\Traits;
 
 
 //use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponse
 {
@@ -16,18 +18,18 @@ trait ApiResponse
 
     public function errorResponse($message, $code)
     {
-        return response()->json(['error'=>$message, 'code'=>$code], $code);
+        return response()->json(['error' => $message, 'code' => $code], $code);
     }
 
     public function showAll(Collection $collection, $code = 200)
     {
-        if ($collection->isEmpty())
-        {
+        if ($collection->isEmpty()) {
             return $this->successResponse(['data' => $collection], $code);
         }
         $transformer = $collection->first()->transformer;
         $collection = $this->filterData($collection, $transformer);
         $collection = $this->sortData($collection, $transformer);
+        $collection = $this->paginate($collection);
         $collection = $this->transforData($collection, $transformer);
         return $this->successResponse($collection, $code);
     }
@@ -39,14 +41,14 @@ trait ApiResponse
         return $this->successResponse($instance, $code);
     }
 
-    public function showMessage($message, $code = 200) {
+    public function showMessage($message, $code = 200)
+    {
         return $this->successResponse(['data' => $message], $code);
     }
 
     protected function filterData(Collection $collection, $transformer)
     {
-        foreach (request()->query() as $query => $value)
-        {
+        foreach (request()->query() as $query => $value) {
             $attribute = $transformer::originalAttribute($query);
 
             if (isset($attribute, $value)) {
@@ -65,6 +67,32 @@ trait ApiResponse
             return $collection;
         }
         return $collection;
+    }
+
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50'
+        ];
+
+        Validator::make(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+
+        $perPage = 15;
+        if (request()->has('per_page')) {
+            $perPage = (int) request()->per_page;
+        }
+
+        $result = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($result, $collection->count(), $perPage, $page,[
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
     }
 
     protected function transforData($data, $tranformer)
